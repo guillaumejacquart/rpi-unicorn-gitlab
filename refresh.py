2c5e157386087aeb4d598b9dd60f9c0eaf3ca647
Guillaume Jacquart <guillaume.jacquart@live.fr>
2017-09-21 15:01:43 +0000

    Add unicorn hat handling

--- refresh.py
+++ refresh.py
@@ -9,6 +9,13 @@ logging.basicConfig()
 from manager.gitlab_projects import project_status
 from manager.db_init import db
 
+import unicornhat as unicorn
+
+
+unicorn.set_layout(unicorn.PHAT)
+unicorn.rotation(0)
+unicorn.brightness(0.5)
+
 def refresh():
     print 'Init projects refresh ...'
     projects = project_status();
@@ -22,9 +29,16 @@ def refresh():
                 color = ('green' if status == 'success' else 'red')
                 strCon = 'light led {},{} with color {}'.format(c[0]['row'], c[0]['col'], color)
                 print strCon
+                if status == 'success': 
+                    unicorn.set_pixel(c[0]['row'], c[0]['col'], 0, 255, 0)
+                else: 
+                    unicorn.set_pixel(c[0]['row'], c[0]['col'], 255, 0, 0)
+            else:
+                unicorn.set_pixel(c[0]['row'], c[0]['col'], 0, 0, 0)
         else:
             db.insert({'name': p, 'row': 0, 'col': 0, 'status': status, 'active': False, 'type': 'project'})
     
+    unicorn.show()
     print 'Done refreshing !'
     
 def init():
